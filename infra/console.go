6d7d95daedd867b60b7514c3645801b893614919
tomocy <tomocy.dev@gmail.com>
2020-05-01 06:28:00 +0900

    update Console
    Console can echo the given args.

--- infra/console.go
+++ infra/console.go
@@ -1,3 +1,23 @@
 package infra
 
+import (
+	"fmt"
+
+	"github.com/tomocy/go-echoargs"
+)
+
 type Console struct{}
+
+func (c Console) Echo(args echoargs.Args) error {
+	vs := args.Values()
+	for i, v := range vs {
+		fmt.Print(v)
+		if i != len(vs)-1 {
+			fmt.Print(", ")
+		}
+	}
+
+	fmt.Println()
+
+	return nil
+}
